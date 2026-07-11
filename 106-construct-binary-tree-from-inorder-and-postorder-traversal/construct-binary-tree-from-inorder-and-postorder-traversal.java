cf81d4d6ccd195d3631565cd8c3cc78ab5e8834c
abhishekjha14 <abhijha1406@gmail.com>
2026-07-11 23:14:07 +0530

    Time: 1 ms (98.86%) | Memory: 45.3 MB (89.24%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,32 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     int p;
+//     HashMap <Integer,Integer> hm=new HashMap<>();
+//     public TreeNode buildTree(int[] inorder, int[] postorder) {
+//         int n= inorder.length;
+//         p=n-1;
+//         for(int i =0;i<n;i++){
+//             hm.put(inorder[i],i);
+//         }
+//         return build(postorder,n-1,0);
+//     }
+//     TreeNode build( int[] postorder,int right, int left){
+//             if(right<left) return null;
+//             if(p<0) return null;
+//             int val = postorder[p--];
+//             TreeNode root=new TreeNode(val);
+//             int mid= hm.get(val);
+//             root.right=build(postorder,right,mid+1);
+//             root.left=build(postorder,mid-1,left);
+//             return root;
+//         }
+// }
 class Solution {
-
-    private int postIdx;
-    private HashMap<Integer, Integer> map;
-
+    private int p, i;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        map = new HashMap<>();
-
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-
-        postIdx = postorder.length - 1;
-        return build(postorder, 0, inorder.length - 1);
+        p = postorder.length - 1;
+        i = inorder.length - 1;
+        return build(postorder, inorder, -3001);
     }
-
-    private TreeNode build(int[] postorder, int left, int right) {
-        if (left > right) {
+    private TreeNode build(int[] postorder, int[] inorder, int stop) {
+        if (p < 0) {
             return null;
         }
-
-        int rootVal = postorder[postIdx--];
-        TreeNode root = new TreeNode(rootVal);
-
-        int mid = map.get(rootVal);
-
-        root.right = build(postorder, mid + 1, right);
-        root.left = build(postorder, left, mid - 1);
-
-        return root;
+        if (i >= 0 && inorder[i] == stop) {
+            i--;
+            return null;
+        }
+        TreeNode node = new TreeNode(postorder[p--]);
+        node.right = build(postorder, inorder, node.val);
+        node.left = build(postorder, inorder, stop);
+        return node;
     }
 }
